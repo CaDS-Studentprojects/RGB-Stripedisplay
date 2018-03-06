9cbe51029e0c7e8d40e80d5895d175c237e7e033
Unknown <mikko.eberhardt@haw-hamburg.de>
2018-03-06 20:52:07 +0100

    added variable for temporary run only 1 day

--- display.py
+++ display.py
@@ -21,6 +21,8 @@ SPI_PORT = 0
 SPI_DEVICE = 0
 pixels = Adafruit_WS2801.WS2801Pixels(PIXEL_COUNT, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE), gpio=GPIO)
 
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
 display = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
            [32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17],
            [33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48],
